54322c1a44849e18ba376432902f38add23def03
Popopond <115360402+Popopond@users.noreply.github.com>
2025-01-25 04:58:19 +0000

    first assigment

--- dags/first_dag/first_dag.py
+++ dags/first_dag/first_dag.py
@@ -13,10 +13,10 @@ default_args = {
 }
 
 dag = DAG(
-    'first_dag',
+    'first_dag',  # name of pipeline should equal file name
     default_args=default_args,
     description='My first Airflow DAG with an empty operator',
-    schedule_interval=timedelta(days=1),
+    schedule_interval=timedelta(days=1),  # pipeline run frequency
     start_date=datetime(2023, 1, 1),
     catchup=False,
 )
@@ -24,15 +24,50 @@ dag = DAG(
 def hello_world():
     print("hello world")
 
+# Dummy intermediate tasks
+def intermediate_task():
+    print("Intermediate task")
+
+def second_intermediate_task():
+    print("Second intermediate task")
+
+# Operators
 start = EmptyOperator(
     task_id='start',
     dag=dag,
 )
 
-hello_world = PythonOperator(
-    task_id='hello-world',
+hello_world_task = PythonOperator(
+    task_id='hello_world',
     dag=dag,
-    python_callable=hello_world
+    python_callable=hello_world,
+)
+
+intermediate_task_op = PythonOperator(
+    task_id='intermediate_task',
+    dag=dag,
+    python_callable=intermediate_task,
+)
+
+second_intermediate_task_op = PythonOperator(
+    task_id='second_intermediate_task',
+    dag=dag,
+    python_callable=second_intermediate_task,
+)
+
+send_email = EmptyOperator(
+    task_id='send_email',
+    dag=dag,
+)
+
+send_ms_team = EmptyOperator(
+    task_id='send_ms_team',
+    dag=dag,
+)
+
+send_line_message = EmptyOperator(
+    task_id='send_line_message',
+    dag=dag,
 )
 
 end = EmptyOperator(
@@ -40,4 +75,9 @@ end = EmptyOperator(
     dag=dag,
 )
 
-start >> hello_world >> end
+# Dependencies
+start >> hello_world_task >> end
+
+start >> [intermediate_task_op,second_intermediate_task_op]  >> end >> [send_ms_team,send_line_message]  
+
+start >> second_intermediate_task_op >> send_email
